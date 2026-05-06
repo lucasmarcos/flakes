ba5d256b6c5bef7c049a133711ed579742272dca
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2026-05-05 23:01:45 -0300

    use steamrt64

--- steam/build-steam-rootfs.nix
+++ steam/build-steam-rootfs.nix
@@ -136,6 +136,8 @@ writeShellApplication {
     echo "steam:x:$(id -g):steam" >> "$ROOTFS/etc/group"
 
     mkdir -p "$ROOTFS/home/steam/.local/share/Steam/package"
+    echo "publicbeta" > "$ROOTFS/home/steam/.local/share/Steam/package/beta"
+    touch "$ROOTFS/home/steam/.local/share/Steam/.steam-enable-steamrt64-client"
 
     echo "en_US.UTF-8 UTF-8" > "$ROOTFS/etc/locale.gen"
     echo "LANG=en_US.UTF-8" > "$ROOTFS/etc/locale.conf"
